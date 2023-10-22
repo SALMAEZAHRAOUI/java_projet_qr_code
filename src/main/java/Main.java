06d3b302d1eb9954378827c23831ba824171fb42
salma <salmaezhraoui@gmail.com>
2023-10-22 10:30:49 +0100

    CODE

---
+++ src/main/java/Main.java
@@ -0,0 +1,86 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+
+public class Main {
+    public static void main(String[] args) throws IOException, WriterException {
+
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+
+        while (!exit) {
+            /*voici le menu */
+            System.out.println("================Menu===============");
+            System.out.println("1. Créer un QR Code");
+            System.out.println("2. Éditer un QR Code");
+            System.out.println("3. Supprimer un QR Code");
+            System.out.println("4. Archiver les QR Codes");
+            System.out.println("0. Quitter");
+            System.out.print("Choisissez une option : ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline character
+
+            switch (choice) {
+                case 1:
+                    createQRCode(scanner);
+                    break;
+                case 2:
+                    // editQRCode(scanner);
+                    break;
+                case 3:
+                    // deleteQRCode(scanner);
+                    break;
+                case 4:
+                    // archiveQRCodes();
+                    break;
+                case 0:
+                    exit = true;
+                    System.out.println("aurevoir ");
+                    break;
+                default:
+                    System.out.println("Option invalide. Veuillez réessayer.");
+                    break;
+            }
+
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+
+
+    public static void createQRCode(Scanner scanner) throws WriterException, IOException {
+        System.out.println("Enter the url https://");
+        String data = scanner.nextLine();
+
+
+        int size = 400;
+
+        // on peut modifier le path
+
+        String filePath = "C:\\Users\\DELL\\Documents\\s3\\Projet_JAVA\\qr_code\\src\\main\\resources\\QRcode-img\\qrcode.png";
+
+
+        BitMatrix bitMatrix = new MultiFormatWriter().encode(data, BarcodeFormat.QR_CODE, size, size);
+
+
+        FileOutputStream fileOutputStream = new FileOutputStream(new File(filePath));
+
+
+        MatrixToImageWriter.writeToStream(bitMatrix, "png", fileOutputStream);
+        System.out.println(fileOutputStream);
+    }
+
+
+
+}
+
